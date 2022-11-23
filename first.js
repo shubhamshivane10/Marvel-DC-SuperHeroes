4efa49d3e0228cca724e70d7ebf32ac9b38ce883
Shubhszzz <85756010+shubhamshivane10@users.noreply.github.com>
2022-11-23 17:56:21 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,37 @@
+let x1= document.getElementById("bt1");
+
+let x2 = function myfun(){
+    window.open("https://www.ign.com/articles/new-dc-movies-and-tv-shows-2022-release-dates-and-beyond");
+}
+
+x1.addEventListener('click',x2);
+
+let x3= document.getElementById("bt2");
+
+let x4 = function myfun(){
+    window.open("https://screenrant.com/marvel-upcoming-movies-list-release-dates/");
+}
+
+x3.addEventListener('click',x4);
+
+
+
+
+
+
+let sams1 = document.getElementById("idd2");
+
+let fun1 = function myfun1(){
+    alert("This tab is yet to be publish");
+}
+
+sams1.addEventListener('click',fun1);
+
+
+let c1 = document.getElementById("ix1");
+console.log(c1);
+let fc = function fc1(){
+    alert("This tab is yet to publish");
+}
+
+c1.addEventListener("click",fc);
---
+++ first1.js
@@ -0,0 +1,57 @@
+let sams2 = document.getElementById("ix1");
+console.log(sams2);
+let fun2 = function myfun1(){
+    alert("This tab is yet to be publish");
+}
+
+sams2.addEventListener('click',fun2);
+
+
+let sams3 = document.getElementById("ix2");
+
+let fun3 = function myfun2(){
+    alert("This tab is yet to be publish");
+}
+
+sams3.addEventListener('click',fun3);
+
+
+let sams4 = document.getElementById("bb1");
+
+
+let fun4 = function myfun3(){
+    window.open("https://www.linkedin.com/in/shubham-shivane-23b263208/");
+}
+
+
+
+
+sams4.addEventListener('click',fun4);
+
+
+let sams5 = document.getElementById("bb2");
+
+
+let fun5 = function myfun3(){
+    window.open(" https://www.facebook.com/shubham.shivne.35");
+}
+
+sams5.addEventListener('click',fun5);
+
+let sams6 = document.getElementById("bb3");
+
+
+let fun6 = function myfun4(){
+    window.open("https://www.instagram.com/shubham_49s/");
+}
+
+sams6.addEventListener('click',fun6);
+
+let sams7 = document.getElementById("bb4");
+
+
+let fun7 = function myfun77(){
+    window.open("https://github.com/shubhamshivane10");
+}
+
+sams7.addEventListener('click',fun7);
